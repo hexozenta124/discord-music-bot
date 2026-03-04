5cfdcb0d5dd23b728d5aec8258f9398d49378c75
hexozenta124 <hexozenta124@gmail.com>
2026-03-04 09:34:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,4 @@
-import os
-import sys
-
-TOKEN = os.getenv("TOKEN")
-
-if not TOKEN:
-    print("❌ ERROR: TOKEN environment variable not found!")
-    sys.exit(1)
+TOKEN = "MTQ3NzMzMTE5NTMwNTEzMjAzMg.GDCUl0.n1KN7ZpSZKceGUyEnquLxStQFOkvJtYcI6DmrE"
 
 EMBED_COLOR = 0x2f3136
 BOT_NAME = "UltraMusic"

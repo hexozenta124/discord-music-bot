af5f4328c25a91bb1eb23163ce59bc5d46575b74
hexozenta124 <hexozenta124@gmail.com>
2026-03-06 00:54:31 +0530

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    TOKEN: process.env.TOKEN,
-    EMBED_COLOR: 0x2f3136,
-    BOT_NAME: "UltraMusic",
-    FOOTER_TEXT: "UltraMusic • Premium Audio Experience",
-    DEFAULT_VOLUME: 0.7
-};
